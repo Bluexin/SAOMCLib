e962995c3140655e3009f22e354477571ccaa225
Tencao <Chris9282@hotmail.co.uk>
2017-08-04 21:16:48 +0100

    Normal version - Temp

--- src/main/java/be/bluexin/saomclib/SAOMCLib.kt
+++ src/main/java/be/bluexin/saomclib/SAOMCLib.kt
@@ -28,7 +28,7 @@ import org.apache.logging.log4j.Logger
 object SAOMCLib {
 
     const val MODID = "saomclib"
-    const val VERSION = "@VERSION@"
+    const val VERSION = "1.2"
     const val DEPENDENCIES = "required-after:forgelin@[1.5.1,)"
 
     @Suppress("unused")
