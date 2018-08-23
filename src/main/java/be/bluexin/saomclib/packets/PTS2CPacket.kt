9e6fb894eeca9702bf3c4cbbfb6038ec8d1af61c
bluexin <bluexin.games@gmail.com>
2018-08-23 23:31:00 +0200

    Bump version
    Signed-off-by: bluexin <bluexin.games@gmail.com>

--- src/main/java/be/bluexin/saomclib/packets/PTS2CPacket.kt
+++ src/main/java/be/bluexin/saomclib/packets/PTS2CPacket.kt
@@ -28,7 +28,6 @@ class PTS2CPacket() : IMessage {
         this.type = type
         this.leader = leader.cachedUniqueIdString
         this.members = members.map { it.cachedUniqueIdString }.toList()
-        println(this.members.joinToString())
     }
 
     override fun fromBytes(buf: ByteBuf) {
