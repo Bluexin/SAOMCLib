9d2a55624782c8df00b8abe9e0022393a02d5020
bluexin <bluexin.games@gmail.com>
2018-08-24 15:34:51 +0200

    This may or may not fix "wildcard" event registration
    Signed-off-by: bluexin <bluexin.games@gmail.com>

--- src/main/java/be/bluexin/saomclib/events/PartyEvent.kt
+++ src/main/java/be/bluexin/saomclib/events/PartyEvent.kt
@@ -8,7 +8,7 @@ import net.minecraft.entity.player.EntityPlayer
  * Party-related events.
  * They are fired both on client and server.
  */
-abstract class PartyEvent(val party: IParty): Event() {
+/*abstract*/open class PartyEvent(val party: IParty) : Event() {
     /**
      * Fired when a player actually joins a party.
      */
